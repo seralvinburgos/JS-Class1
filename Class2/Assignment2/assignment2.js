197dd4e7bf2986ccf0246a68e7e2cabd40826c1a
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2022-08-24 09:45:53 -0400

    JS Assignment2 updated grid
    updated containerMid. assigned specific grid areas to each box. Unable to assign js variable prompt.

---
+++ Class2/Assignment2/assignment2.js
@@ -0,0 +1,11 @@
+let product1 = prompt("Enter product description: ");
+let product1Quantity = Number(prompt("Enter quantity: "));
+let product1Price = Number(prompt("Enter price: "));
+let product2 = prompt("Enter product description: ");
+let product2Quantity = Number(prompt("Enter quantity: "));
+let product2Price = Number(prompt("Enter price: "));
+let product3 = prompt("Enter product description: ");
+let product3Quantity = Number(prompt("Enter quantity: "));
+let product3Price = Number(prompt("Enter price: "));
+
+
