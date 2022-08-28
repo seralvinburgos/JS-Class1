c4c345eee8e175c54db4daa02c967932dafd7d43
seralvinburgos <110437441+seralvinburgos@users.noreply.github.com>
2022-08-27 22:33:20 -0400

    JS-Class 4 Final Report Competency (final)
    Final changes made. Ready for submission.

--- Class2/JS Class 4/JS Class 4 Competency/app.js
+++ Class2/JS Class 4/JS Class 4 Competency/app.js
@@ -1,5 +1,8 @@
 const taskInput=document.getElementById("task");
+const taskInput2=document.getElementById("task2");
 let c=0;
+
+/* Priority checklist */
 
 function addTask(){
     let task=taskInput.value;
@@ -10,7 +13,7 @@ function addTask(){
 }
 function displayTask(newTask){
     document.getElementById("task-list").innerHTML += `
-    <li id=${c}>${newTask} <button onclick="deleteTask(${c})">Delete </button></li>`;
+    <li id=${c}>${newTask} <button id="deleteButton" onclick="deleteTask(${c})">X </button></li>`;
 }
 function clearTask(task){
     taskInput.value="";
@@ -19,5 +22,26 @@ function clearTask(task){
 function deleteTask(taskID){
     document.getElementById(taskID).remove();
     console.log("Deleting task" + taskID);
+}
 
+/* General checklist */
+
+function addTask2(){
+    let task2=taskInput2.value;
+    console.log(task2);
+    displayTask2(task2);
+    clearTask2(task2);
+    c++;
+}
+function displayTask2(newTask2){
+    document.getElementById("task-list2").innerHTML += `
+    <li id=${c}>${newTask2} <button id="deleteButton" onclick="deleteTask(${c})">X </button></li>`;
+}
+function clearTask2(task2){
+    taskInput2.value="";
+    taskInput2.focus();
+}
+function deleteTask2(taskID2){
+    document.getElementById(taskID2).remove();
+    console.log("Deleting task" + taskID2);
 }
